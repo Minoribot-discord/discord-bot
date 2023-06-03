36bfb007daab4ace3f426f9581b2dc954009cdd1
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-03 17:13:19 +0200

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-// export * from "https://deno.land/x/amethyst@v7.0.0/mod.ts";
-export * as dotenv from "https://deno.land/std@0.177.0/dotenv/mod.ts";
+export * as dotenv from "https://deno.land/std@0.190.0/dotenv/mod.ts";
 export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 export * from "https://deno.land/x/discordeno@18.0.1/plugins/mod.ts";
-export * as log from "https://deno.land/std@0.177.0/log/mod.ts";
-export * as fs from "https://deno.land/std@0.177.0/fs/mod.ts";
+export * as log from "https://deno.land/std@0.190.0/log/mod.ts";
+export * as fs from "https://deno.land/std@0.190.0/fs/mod.ts";
+export * as mongo_atlas_sdk from "https://deno.land/x/atlas_sdk@v1.1.1/mod.ts";
