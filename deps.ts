16964b18cf0a937a7910546c44da11068dd3e9aa
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-05 20:39:45 +0200

    deps: add std@0.190.0/flags

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
+export * as fs from "https://deno.land/std@0.190.0/fs/mod.ts";
+export * as log from "https://deno.land/std@0.190.0/log/mod.ts";
 export * as dotenv from "https://deno.land/std@0.190.0/dotenv/mod.ts";
+export * as flags from "https://deno.land/std@0.190.0/flags/mod.ts";
 export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 export * from "https://deno.land/x/discordeno@18.0.1/plugins/mod.ts";
-export * as log from "https://deno.land/std@0.190.0/log/mod.ts";
-export * as fs from "https://deno.land/std@0.190.0/fs/mod.ts";
 export * as mongo_atlas_sdk from "https://deno.land/x/atlas_sdk@v1.1.1/mod.ts";
