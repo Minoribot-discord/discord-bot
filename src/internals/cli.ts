e9ea638b7bb2502a3e61c1653fdad377cacbaa61
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-14 19:06:59 +0200

    feat(cli): impl --dev and aliases -r & -d

--- src/internals/cli.ts
+++ src/internals/cli.ts
@@ -1,11 +1,24 @@
 import { flags } from "deps";
 import { CustomBot } from "./CustomBotType.ts";
-type args = flags.Args & { refreshcommands?: boolean };
+type args = flags.Args & {
+  refreshcommands?: boolean;
+  r?: boolean;
+  dev?: boolean;
+  d?: boolean;
+};
 
 function readStartupCommandLineArgs(bot: CustomBot) {
   const parsedArgs = flags.parse<args>(Deno.args);
-  if (parsedArgs.refreshcommands) {
-    bot.config.refreshCommands = parsedArgs.refreshcommands;
+
+  if (parsedArgs.refreshcommands || parsedArgs.r) {
+    bot.config.refreshCommands = parsedArgs.refreshcommands ||
+      parsedArgs.r ||
+      false;
+  }
+  if (parsedArgs.dev || parsedArgs.d) {
+    bot.config.devMode = parsedArgs.dev ||
+      parsedArgs.d ||
+      false;
   }
 }
 
