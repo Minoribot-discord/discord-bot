37f690fef7d1b81b729069b873244f1df44c6bd5
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-14 19:13:51 +0200

    feat(src/mod.ts) added a little warning for dev mode

--- src/mod.ts
+++ src/mod.ts
@@ -8,6 +8,12 @@ import { loadInhibitors } from "./internals/loadInhibitors.ts";
 // read the args passed to the script at startup and handle them
 readStartupCommandLineArgs(customBot);
 
+if (customBot.config.devMode) {
+  customBot.logger.warning(
+    "Dev mode enabled! Procede with caution, and except some changes in behaviour.",
+  );
+}
+
 // load all the inhibitors
 // they're basically filters/conditions for commands
 await loadInhibitors(customBot);
@@ -19,5 +25,5 @@ await loadCommands(customBot);
 await loadModules(customBot);
 
 // start the bot
-customBot.logger.info("Starting bot");
+customBot.logger.info("Starting connection to the Discord API & gateway");
 await startBot(customBot);
