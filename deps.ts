73100fa94e5c823b16c71b4de3f8e07fa202509e
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-16 17:51:45 +0200

    fix(deps): change the way mongo atlas is exported

--- deps.ts
+++ deps.ts
@@ -4,4 +4,8 @@ export * as dotenv from "https://deno.land/std@0.190.0/dotenv/mod.ts";
 export * as flags from "https://deno.land/std@0.190.0/flags/mod.ts";
 export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 export * from "https://deno.land/x/discordeno@18.0.1/plugins/mod.ts";
-export * as mongo_atlas_sdk from "https://deno.land/x/atlas_sdk@v1.1.1/mod.ts";
+export {
+  Collection as MongoCollection,
+  MongoClient,
+  ObjectId,
+} from "https://deno.land/x/atlas_sdk@v1.1.1/mod.ts";
