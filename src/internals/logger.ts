dbd465179c839fbbfc44080a1fe5ff2149b7d365
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-18 21:54:32 +0200

    feat/fix(logger): fix fileHandler, move logs into ./logs

--- src/internals/logger.ts
+++ src/internals/logger.ts
@@ -1,14 +1,27 @@
-import { log } from "deps";
+import { fs, log } from "deps";
+import { formatTime } from "utils";
+
+const logDirectory = "logs";
+
+await fs.ensureDir(`./${logDirectory}`);
+
+const generateFileName = () => {
+  return `./${logDirectory}/log_${formatTime(new Date())}.txt`;
+};
+
+const fileHandler = new log.handlers.FileHandler("DEBUG", {
+  filename: generateFileName(),
+
+  formatter: ({ levelName, msg, datetime }) =>
+    `[${formatTime(datetime, " | ")}] - ${levelName} ${msg}`,
+});
 
 log.setup({
   handlers: {
     console: new log.handlers.ConsoleHandler("DEBUG", {
       formatter: "{levelName} {msg}",
     }),
-    file: new log.handlers.FileHandler("DEBUG", {
-      filename: "./log.txt",
-      formatter: "{levelName} {msg}",
-    }),
+    file: fileHandler,
   },
   loggers: {
     default: {
@@ -19,5 +32,16 @@ log.setup({
 });
 
 const logger = log.getLogger();
+
+const flushFn = () => {
+  fileHandler.flush();
+  fileHandler;
+};
+setInterval(flushFn, 2 * 1000);
+
+globalThis.addEventListener("unload", () => {
+  console.log("fileHandler flushed");
+  fileHandler.flush();
+});
 
 export { logger };
