33bfe027a31aefa816715eacefc8a65682d136d8
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-19 12:49:25 +0200

    fix(utils): forgot to commit this

--- src/internals/utils/utils.ts
+++ src/internals/utils/utils.ts
@@ -1,1 +1,26 @@
 export * from "./fetch.ts";
+
+const formatTime = (date: Date, separator = "_"): string => {
+  const timePart = formatTimePart(date);
+  const datePart = formatDatePart(date);
+
+  return `${timePart}${separator}${datePart}`;
+};
+
+const formatTimePart = (date: Date): string => {
+  const hour = date.getHours().toString().padStart(2, "0");
+  const minute = date.getMinutes().toString().padStart(2, "0");
+  const second = date.getSeconds().toString().padStart(2, "0");
+
+  return `${hour}-${minute}-${second}`;
+};
+
+const formatDatePart = (date: Date): string => {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear().toString();
+
+  return `${day}-${month}-${year}`;
+};
+
+export { formatDatePart, formatTime, formatTimePart };
