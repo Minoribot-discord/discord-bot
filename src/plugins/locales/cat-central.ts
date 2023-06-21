54c459263fc7ff2c54610e68af365165c8ffbd47
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-21 18:24:19 +0200

    chore(cat-central): change name prop to "Català central estàndard"

--- src/plugins/locales/cat-central.ts
+++ src/plugins/locales/cat-central.ts
@@ -1,6 +1,6 @@
 const cat_central = {
   code: "cat-central",
-  name: "Català central",
+  name: "Català central estàndard",
   keys: {
     "COMMAND.APP.INFO.INFOEMBED.TITLE":
       "Vet aquí informacions sobre el bot Discord:",
