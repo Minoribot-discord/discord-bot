e9daecdd96265783cab8ecc617a4432dc6e09e58
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-23 23:43:17 +0200

    refactor: all files ending with "mod.ts" renamed "mod.ts"

--- src/internals/database/database_mod.ts
+++
@@ -1,3 +0,0 @@
-export * from "./databases/databases_mod.ts";
-export * from "./structures/db_structures_mod.ts";
-export * from "./database.ts";
---
+++ src/internals/database/mod.ts
@@ -0,0 +1,3 @@
+export * from "./databases/mod.ts";
+export * from "./structures/mod.ts";
+export * from "./database.ts";
