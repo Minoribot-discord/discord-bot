dec7c9a4cc9b19042f7249fb083f809423be8ae3
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-06-30 15:35:28 +0200

    feat(commands/info): more info messages, ram

--- src/plugins/commands/bot/info.ts
+++ src/plugins/commands/bot/info.ts
@@ -6,13 +6,21 @@ const execute: CommandExecuteFunc = async (
   ctx,
   i18n,
 ) => {
+  const systemMemoryInfo = Deno.systemMemoryInfo();
+
   const infoEmbed: Embed = {
     title: i18n.translate("COMMAND.APP.INFO.EMBED.TITLE"),
     fields: [{
       name: "Caches",
       value: `**Users:** ${ctx.bot.users.size}\n` +
         `**Channels:** ${ctx.bot.channels.size}\n` +
-        `**Guilds:** ${ctx.bot.guilds.size}\n`,
+        `**Guilds:** ${ctx.bot.guilds.size}\n` +
+        `**Messages:** ${ctx.bot.messages.size}\n`,
+    }, {
+      name: "Resource consumption",
+      value: `**Ram:** ${
+        (Deno.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)
+      }/${(systemMemoryInfo.total / 1024).toFixed(0)} MB`,
     }],
   };
   await ctx.reply({ embeds: [infoEmbed] });
