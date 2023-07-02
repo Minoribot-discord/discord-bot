806d8976f441d9c78bb4b642c939ef57ac589404
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-07-02 22:12:24 +0200

    feat(structures): Errors.ts for custom errors Already added CommandError

--- src/internals/structures/mod.ts
+++ src/internals/structures/mod.ts
@@ -2,6 +2,7 @@ export * from "./context/mod.ts";
 export * from "./Collector.ts";
 export * from "./Command.ts";
 export * from "./CommandCategory.ts";
+export * from "./Errors.ts";
 export * from "./Inhibitor.ts";
 export * from "./Locale.ts";
 export * from "./Module.ts";
