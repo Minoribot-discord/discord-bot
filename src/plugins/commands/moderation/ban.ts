bd863ab79d5200e26fa7beb78eda6d22a142c906
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-07-03 19:14:32 +0200

    feat(commands): add ban command feat(locals/cat-central): add the associated keys chore(README): deno fmt

---
+++ src/plugins/commands/moderation/ban.ts
@@ -0,0 +1,99 @@
+import {
+  ApplicationCommandOption,
+  ApplicationCommandOptionTypes,
+  Embed,
+  User,
+} from "deps";
+import { createCommand } from "internals/loadStuff.ts";
+import { CommandError, CommandScope } from "structures";
+import { getOrFetchUser, parseTime, userUsernameAndDiscriminator } from "utils";
+
+const options: ApplicationCommandOption[] = [
+  {
+    name: "user",
+    description: "The user or user id to ban",
+    type: ApplicationCommandOptionTypes.User,
+    required: true,
+  },
+  {
+    name: "reason",
+    description: "The reason for the ban",
+    type: ApplicationCommandOptionTypes.String,
+    minLength: 1,
+    required: false,
+  },
+  {
+    name: "delete-message-history",
+    description: "Delete message history of the user (Example: 2d, 10h5m)",
+    type: ApplicationCommandOptionTypes.String,
+    minLength: 2,
+    required: false,
+  },
+  {
+    name: "visible",
+    description: "Show the ban message or not",
+    type: ApplicationCommandOptionTypes.Boolean,
+    required: false,
+  },
+];
+
+createCommand({
+  name: "ban",
+  category: "moderation",
+  description: "Ban a user from the server",
+  scope: CommandScope.GLOBAL,
+  dmPermission: false,
+  options,
+  execute: async (ctx, i18n) => {
+    const _userIdToBan = ctx.args.getString("user");
+    if (!_userIdToBan) {
+      throw new CommandError("No 'user' was provided??");
+    }
+    const userIdToBan: bigint = ctx.bot.transformers.snowflake(_userIdToBan);
+
+    const reason = ctx.args.getString("reason");
+
+    const deleteMessageHistory = ctx.args.getString("delete-message-history");
+    let parsedDeleteMessageHistoryTime: number | null | undefined = undefined;
+    if (deleteMessageHistory) {
+      parsedDeleteMessageHistoryTime = parseTime(deleteMessageHistory);
+      if (parsedDeleteMessageHistoryTime === null) {
+        return void await ctx.reply(
+          i18n.translate("COMMAND.APP.BAN.PROVIDE_CORRECT_DELETE_MSG_HISTORY", [
+            "(e.g.: 1d, 10m, 4h)",
+          ]),
+          { private: true },
+        );
+      }
+    }
+
+    const user = await getOrFetchUser(ctx.bot, userIdToBan);
+
+    await ctx.bot.helpers.banMember(
+      ctx.guildId!,
+      userIdToBan,
+      {
+        deleteMessageSeconds: parsedDeleteMessageHistoryTime,
+        reason,
+      },
+    );
+
+    const isBanMessageVisible = ctx.args.getBoolean("visible") ?? false;
+    const banEmbed: Embed = {
+      title: i18n.translate("COMMAND.APP.BAN.BANEMBED.TITLE"),
+      fields: [
+        {
+          name: i18n.translate("COMMAND.APP.BAN.BANEMBED.FIELDS.USER"),
+          value: `\`${userUsernameAndDiscriminator(user)}\`` +
+            `\n<@${user.id}>\n\`${user.id}\``,
+        },
+        {
+          name: i18n.translate("COMMAND.APP.BAN.BANEMBED.FIELDS.REASON"),
+          value: `\`${reason}\`` ??
+            i18n.translate("COMMAND.APP.BAN.BANEMBED.FIELDS.REASON.NO_REASON"),
+        },
+      ],
+    };
+    await ctx.reply({ embeds: [banEmbed] }, { private: !isBanMessageVisible });
+  },
+});
--- src/plugins/locales/cat-central.ts
+++ src/plugins/locales/cat-central.ts
@@ -4,16 +4,20 @@ const cat_central = {
   code: "cat-central",
   name: "Català central estàndard",
   keys: {
+    // Collectors
     "COLLECTORS.GLOBAL.REJECT.COLLECTOR_BEGAN_BEFORE":
       "Un nou col·lector ha començat abans que l'usuari respongui a l'antic col·lector.",
     "COLLECTORS.COMPONENTS.REJECT.TIMEOUT":
       "No has utilitzat el component a temps.",
     "COLLECTORS.REACTIONS.REJECT.TIMEOUT": "No has reaccionat a temps.",
     "COLLECTORS.MESSAGES.REJECT.TIMEOUT": "No has enviat un missatge a temps.",
+    // Application commands
+    // Info command
     "COMMAND.APP.INFO.EMBED.TITLE":
       "Vet aquí informacions sobre el bot de Discord:",
     "COMMAND.APP.SETTINGS.SERVER.CONFIGEMBED.TITLE":
       "Configuració del servidor",
+    // Settings command
     "COMMAND.APP.SETTINGS.SERVER.CONFIGEMBED.FIELDS.LANGUAGE.NAME": "Llengua",
     "COMMAND.APP.SETTINGS.SERVER.CONFIGEMBED.FIELDS.LANGUAGE.VALUE.DEFAULT_LANG":
       (
@@ -21,6 +25,13 @@ const cat_central = {
       ) => `La llenga per defecte (${langName})`,
     "COMMAND.APP.SETTINGS.SERVER.NEWLANGUAGE": (formattedLangName: string) =>
       `Llengua configurada amb èxit - ${formattedLangName}`,
+    // Ban command
+    "COMMAND.APP.BAN.PROVIDE_CORRECT_DELETE_MSG_HISTORY": (examples: string) =>
+      `Necessiteu fornir un temps correcte. ${examples}`,
+    "COMMAND.APP.BAN.BANEMBED.TITLE": "Usuari bandejat",
+    "COMMAND.APP.BAN.BANEMBED.FIELDS.USER": "Usuari",
+    "COMMAND.APP.BAN.BANEMBED.FIELDS.REASON": "Motiu",
+    "COMMAND.APP.BAN.BANEMBED.FIELDS.REASON.NO_REASON": "Cap",
   },
 };
 
