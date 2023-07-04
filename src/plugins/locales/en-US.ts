5bbb877f9f5391e361a1a79d681ef24deb1194a7
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-07-04 02:55:50 +0200

    fix(en-US): add missing fields

--- src/plugins/locales/en-US.ts
+++ src/plugins/locales/en-US.ts
@@ -4,12 +4,15 @@ loadLocale({
   code: "en-US",
   name: "American English",
   keys: {
+    // Collectors
     "COLLECTORS.GLOBAL.REJECT.COLLECTOR_BEGAN_BEFORE":
       "A new collector began before the user responded to the previous one.",
     "COLLECTORS.COMPONENTS.REJECT.TIMEOUT":
       "You didn't use the component in time.",
     "COLLECTORS.REACTIONS.REJECT.TIMEOUT": "You didn't react in time.",
     "COLLECTORS.MESSAGES.REJECT.TIMEOUT": "You didn't send a message in time.",
+    // Application commands
+    // Info command
     "COMMAND.APP.INFO.EMBED.TITLE": "Here are some information about the bot:",
     "COMMAND.APP.SETTINGS.SERVER.CONFIGEMBED.TITLE": "Server configuration",
     "COMMAND.APP.SETTINGS.SERVER.CONFIGEMBED.FIELDS.LANGUAGE.NAME": "Language",
@@ -19,5 +22,14 @@ loadLocale({
       ) => `Default language (${langName})`,
     "COMMAND.APP.SETTINGS.SERVER.NEWLANGUAGE": (formattedLangName: string) =>
       `Language configurated successfully - ${formattedLangName}`,
+    // Ban command
+    "COMMAND.APP.BAN.PROVIDE_CORRECT_DELETE_MSG_HISTORY": (examples: string) =>
+      `You need to provide a correct time. ${examples}`,
+    "COMMAND.APP.BAN.BANEMBED.TITLE": "User banned",
+    "COMMAND.APP.BAN.BANEMBED.FIELDS.USER": "User",
+    "COMMAND.APP.BAN.BANEMBED.FIELDS.REASON": "Reason",
+    "COMMAND.APP.BAN.BANEMBED.FIELDS.REASON.NO_REASON": "None",
+    // Kick command
+    "COMMAND.APP.KICK.KICKEMBED.TITLE": "User kicked",
   },
 });
