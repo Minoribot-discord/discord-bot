70475f0f51b098d72adc9c748b446c9d15e05cfb
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-07-15 00:21:43 +0200

    fix(./src/mod.ts): fix fetch recommended by someone on Discord, might fix something with Discordeno v19 (smth to do with components?)

--- src/mod.ts
+++ src/mod.ts
@@ -1,16 +1,36 @@
+import { snakelize } from "deps";
 import { botConfig } from "config";
 import { customBot } from "bot";
 import { readStartupCommandLineArgs } from "internals/cli.ts";
 import { start } from "./start.ts";
-
 // read the args passed to the script at startup and handle them
 readStartupCommandLineArgs();
 
 if (botConfig.devMode) {
-  customBot.logger.warning(
+  customBot.logger.warn(
     "Dev mode enabled! Procede with caution, and except some changes in behaviour.",
   );
 }
+
+const baseFetch = globalThis.fetch;
+globalThis.fetch = (url, data) => {
+  const sUrl = url.toString();
+  if (
+    sUrl.includes("api/v10/webhooks/") || sUrl.includes("api/v10/interactions")
+  ) {
+    if (data?.body && data.body instanceof FormData) {
+      const jsonRaw = data.body.get("payload_json")?.toString();
+      if (jsonRaw) {
+        data.body.set(
+          "payload_json",
+          JSON.stringify(snakelize(JSON.parse(jsonRaw))),
+        );
+      }
+    }
+  }
+
+  return baseFetch(url, data);
+};
 
 // Little script to convert the enum BitwisePermissionFlags to a object, filtering any non-string key
 
