ff0d85c9c22c8d5bcceb8c03bd177740dc6cdbbd
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-07-15 00:14:18 +0200

    deps: update discordeno v19, add zod & redis

--- deps.ts
+++ deps.ts
@@ -2,14 +2,10 @@ export * as fs from "https://deno.land/std@0.192.0/fs/mod.ts";
 export * as log from "https://deno.land/std@0.192.0/log/mod.ts";
 export * as dotenv from "https://deno.land/std@0.192.0/dotenv/mod.ts";
 export * as flags from "https://deno.land/std@0.192.0/flags/mod.ts";
-export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
+export * from "npm:@discordeno/utils";
+export * from "npm:@discordeno/bot";
+// export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 export * from "https://deno.land/x/discordeno@18.0.1/plugins/mod.ts";
-// export {
-//   Collection as MongoCollection,
-//   Database,
-//   MongoClient,
-// } from "https://deno.land/x/atlas_sdk@v1.1.1/client.ts";
-// export { ObjectId } from "https://deno.land/x/atlas_sdk@v1.1.1/deps.ts";
 
 export {
   Collection as MongoCollection,
@@ -17,6 +13,9 @@ export {
   MongoClient,
   ObjectId,
 } from "https://deno.land/x/mongo@v0.31.2/mod.ts";
+
+export { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
+export * from "https://esm.sh/redis@4.6.7";
 
 // @deno-types="npm:@types/lodash@^4"
 import lodash from "npm:lodash@^4";
