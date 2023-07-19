9a14dcf97584c2577ce842e4023fbc0629471c18
Laura <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-07-19 19:50:08 +0000

    deps: specify more precise versions for NPM dependencies

--- deps.ts
+++ deps.ts
@@ -2,10 +2,15 @@ export * as fs from "https://deno.land/std@0.192.0/fs/mod.ts";
 export * as log from "https://deno.land/std@0.192.0/log/mod.ts";
 export * as dotenv from "https://deno.land/std@0.192.0/dotenv/mod.ts";
 export * as flags from "https://deno.land/std@0.192.0/flags/mod.ts";
-export * from "npm:@discordeno/utils";
-export * from "npm:@discordeno/bot";
+export * from "npm:@discordeno/utils@19.0.0-next.8d1ba2a";
+export * from "npm:@discordeno/bot@19.0.0-next.8d1ba2a";
 // export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
-export * from "https://deno.land/x/discordeno@18.0.1/plugins/mod.ts";
+// export {
+//   enableCachePlugin,
+//   hasChannelPermissions,
+//   hasGuildPermissions,
+//   highestRole,
+// } from "https://deno.land/x/discordeno@18.0.1/plugins/mod.ts";
 
 export {
   Collection as MongoCollection,
@@ -17,7 +22,7 @@ export {
 export { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
 export * from "https://esm.sh/redis@4.6.7";
 
-// @deno-types="npm:@types/lodash@^4"
-import lodash from "npm:lodash@^4";
+// @deno-types="npm:@types/lodash@4.14.195"
+import lodash from "npm:lodash@4.17.21";
 
 export { lodash };
