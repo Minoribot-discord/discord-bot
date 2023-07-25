7a0687d73eb832ba9ccbf29722999f6fbaa9a2aa
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-07-25 18:08:37 +0200

    refactor(cache): almost completely remove Redis cache code later i'll add it back with a better design

--- src/cache/createCache.ts
+++ src/cache/createCache.ts
@@ -5,11 +5,6 @@ import {
   Channel,
   Collection,
   createClient,
-  DiscordChannel,
-  DiscordGuild,
-  DiscordMember,
-  DiscordMessage,
-  DiscordUser,
   Guild,
   logger,
   Member,
@@ -18,9 +13,8 @@ import {
   User,
 } from "deps";
 import { discordConfig, redisConfig } from "config";
-import { Serializer } from "./types.ts";
 
-// Todo: Finish serializers and maybe implement Redis as main cache
+// Todo: Implement Redis cache (using an encoder & a decoder)
 export const redisCacheClient = createClient({ url: redisConfig?.cacheUrl });
 // export async function initRedisCacheClient() {
 //   logger.info("Connecting to the Redis cache database");
@@ -30,28 +24,25 @@ export const redisCacheClient = createClient({ url: redisConfig?.cacheUrl });
 
 function createCache<
   // deno-lint-ignore no-explicit-any
-  TDeserialized extends Record<string, any>,
-  // deno-lint-ignore no-explicit-any
-  TSerialized extends Record<string, any>,
+  TData extends Record<string, any>,
 >(
   options: {
-    namespace: string;
-    serializer?: Serializer<TDeserialized, TSerialized>;
+    cacheName: string;
     filter?: (
-      value: TDeserialized,
+      value: TData,
       cachedAt: Collection<bigint, number>,
     ) => boolean;
     interval?: number;
   },
 ) {
-  const { namespace, filter, interval } = options;
-  const collection = new Collection<string, TDeserialized>();
+  const { cacheName, filter, interval } = options;
+  const collection = new Collection<string, TData>();
   const cachedAt = new Collection<bigint, number>();
 
   collection.startSweeper({
     filter: filter
       ? ((value) => {
-        logger.info(`Cleaned cache ${namespace}`);
+        logger.info(`Cleaned cache ${cacheName}`);
         return filter(value, cachedAt);
       })
       : (() => true),
@@ -62,52 +53,17 @@ function createCache<
   }
 
   return {
-    async set(key: BigString, value: TDeserialized): Promise<void> {
+    async set(key: BigString, value: TData): Promise<void> {
       resetCachedAt(BigInt(key));
       if (typeof key === "bigint") key = bigintToSnowflake(key);
 
-      return void collection.set(key, value);
-
-      /*
-      For now i'll cache in ram, but later on I might cache in Redis
-      So I'll let this commented code
-      */
-      // const ok = await redisCacheClient.json.set(
-      //   `${namespace}:${key}`,
-      //   "$",
-      //   serializer?.serialize
-      //     ? jsonSchema.parse(
-      //       removeUndefinedValuesFromObject(serializer.serialize(value)),
-      //     )
-      //     : (removeUndefinedValuesFromObject(value) as unknown) as TSerialized,
-      // );
-
-      // if (ok !== "OK") {
-      //   throw new Error(
-      //     "Couldn't set the value correctly, Redis server didn't return 'OK'",
-      //   );
-      // }
+      collection.set(key, value);
     },
-    async get(key: BigString): Promise<TDeserialized | null> {
+    async get(key: BigString): Promise<TData | null> {
       resetCachedAt(BigInt(key));
       if (typeof key === "bigint") key = bigintToSnowflake(key);
 
       return collection.get(key) ?? null;
-
-      /*
-      For now i'll cache in ram, but later on I might cache in Redis
-      So I'll let this commented code
-      */
-      // const value = (await redisCacheClient.json.get(
-      //   `${namespace}:${key}`,
-      // )) as TSerialized | null;
-      // if (!value) return null;
-
-      // return serializer?.deserialize
-      //   ? serializer.deserialize(
-      //     (jsonSchema.parse(value) as unknown) as TSerialized,
-      //   )
-      //   : (value as unknown) as TDeserialized;
     },
 
     async size(): Promise<number> {
@@ -126,8 +82,7 @@ function createCache<
       cachedAt.delete(snowflakeToBigint(key));
       if (typeof key === "bigint") key = bigintToSnowflake(key);
 
-      return void collection.delete(key);
-      //  await redisCacheClient.json.del(`${namespace}:${key}`);
+      collection.delete(key);
     },
   };
 }
@@ -137,8 +92,8 @@ export type RedisCache = ReturnType<typeof createCache>;
 export const customCache = {
   existingGuilds: new Set<bigint>(),
   // unavailableGuilds: new Set<bigint>(),
-  users: createCache<User, DiscordUser>({
-    namespace: "users",
+  users: createCache<User>({
+    cacheName: "users",
     filter: (user, cachedAt) => {
       return !user.bot &&
         (cachedAt.has(user.id)
@@ -149,8 +104,8 @@ export const customCache = {
   /**
    * Key needs to follow this format: `${guildId}${memberId}`
    */
-  members: createCache<Member, DiscordMember>({
-    namespace: "members",
+  members: createCache<Member>({
+    cacheName: "members",
     filter: (member, cachedAt) => {
       const resolvedId = snowflakeToBigint(`${member.guildId}${member.id}`);
       return (member.user ? !member.user.bot : true) &&
@@ -160,8 +115,8 @@ export const customCache = {
     },
     interval: 10 * 1000 * 60,
   }),
-  guilds: createCache<Guild, DiscordGuild>({
-    namespace: "guilds",
+  guilds: createCache<Guild>({
+    cacheName: "guilds",
     filter: (guild, cachedAt) => {
       return (guild.id !== discordConfig.supportGuildId) &&
         (cachedAt.has(guild.id)
@@ -170,15 +125,15 @@ export const customCache = {
     },
     interval: 15 * 1000 * 60,
   }),
-  channels: createCache<Channel, DiscordChannel>({
-    namespace: "channels",
+  channels: createCache<Channel>({
+    cacheName: "channels",
     filter: (channel, cachedAt) => {
       return (cachedAt.has(channel.id)
         ? (Date.now() - cachedAt.get(channel.id)! > 15 * 1000 * 60)
         : true);
     },
   }),
-  messages: createCache<Message, DiscordMessage>({
-    namespace: "messages",
+  messages: createCache<Message>({
+    cacheName: "messages",
   }),
 };
--- src/cache/types.ts
+++ src/cache/types.ts
@@ -13,13 +13,3 @@ export type RedisJSON =
   | Date
   | RedisJSONArray
   | RedisJSONObject;
-
-export interface Serializer<
-  // deno-lint-ignore no-explicit-any
-  TDeserialized extends Record<string, any>,
-  // deno-lint-ignore no-explicit-any
-  TSerialized extends Record<string, any>,
-> {
-  serialize?: (input: TDeserialized) => Partial<TSerialized>;
-  deserialize?: (input: TSerialized) => TDeserialized;
-}
