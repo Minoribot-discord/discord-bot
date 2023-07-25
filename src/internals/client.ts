da71c2f4cea2de61f10d10bfaf09af449c76e8e5
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-07-25 18:02:07 +0200

    refactor(client): function createCustomBot for adding custom properties

--- src/internals/client.ts
+++ src/internals/client.ts
@@ -1,4 +1,4 @@
-import { createBot } from "deps";
+import { Bot, createBot } from "deps";
 import { botConfig, discordConfig } from "config";
 import { CustomBot, I18nHandler, tasks } from "internals";
 import { collectors, getOrFetchUser } from "utils";
@@ -16,22 +16,27 @@ const { token, intents } = discordConfig;
 const baseBot = createBot({ token, intents, events: {} });
 
 // implement the custom properties to the bot object
-const customBot = baseBot as CustomBot;
-customBot.ready = false;
-customBot.config = botConfig;
-customBot.ownerId = discordConfig.ownerId;
-customBot.getBotUser = () => getOrFetchUser(customBot, customBot.id);
-customBot.i18n = new I18nHandler(customBot);
-customBot.collectors = collectors;
-customBot.modules = modules;
-customBot.commands = commands;
-customBot.subCommands = subCommands;
-customBot.cmdCategories = commandCategories;
-customBot.locales = locales;
-customBot.inhibitors = inhibitors;
-customBot.tasks = tasks;
-customBot.runningTasks = runningTasks;
+export const customBot = createCustomBot(baseBot);
 
 export const logger = customBot.logger;
 
-export { customBot };
+function createCustomBot(bot: Bot): CustomBot {
+  const customBot = bot as CustomBot;
+
+  customBot.ready = false;
+  customBot.config = botConfig;
+  customBot.ownerId = discordConfig.ownerId;
+  customBot.getBotUser = () => getOrFetchUser(customBot, customBot.id);
+  customBot.i18n = new I18nHandler(customBot);
+  customBot.collectors = collectors;
+  customBot.modules = modules;
+  customBot.commands = commands;
+  customBot.subCommands = subCommands;
+  customBot.cmdCategories = commandCategories;
+  customBot.locales = locales;
+  customBot.inhibitors = inhibitors;
+  customBot.tasks = tasks;
+  customBot.runningTasks = runningTasks;
+
+  return customBot;
+}
