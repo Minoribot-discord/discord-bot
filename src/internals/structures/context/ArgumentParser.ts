7abdf279cec3f50eda24ebb31bf438e0f5601018
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-08-19 16:34:22 +0200

    refactor(ArgumentParser): remove getMember, getChannel and getRole methods

--- src/internals/structures/context/ArgumentParser.ts
+++ src/internals/structures/context/ArgumentParser.ts
@@ -1,9 +1,4 @@
-import {
-  DiscordChannel,
-  DiscordMember,
-  DiscordRole,
-  InteractionDataOption,
-} from "deps";
+import { InteractionDataOption } from "deps";
 
 export class ArgumentParser {
   constructor(public raw: InteractionDataOption[] | undefined) {}
@@ -24,26 +19,5 @@ export class ArgumentParser {
     return this.raw?.find((arg) =>
       (arg.name === argName) && (typeof arg.value === "boolean")
     )?.value as boolean | undefined;
-  }
-
-  getMember(argName: string): DiscordMember | undefined {
-    return this.raw?.find((arg) =>
-      (arg.name === argName) &&
-      ((typeof arg.value === "object") && ("roles" in arg.value))
-    )?.value as DiscordMember | undefined;
-  }
-
-  getChannel(argName: string): DiscordChannel | undefined {
-    return this.raw?.find((arg) =>
-      (arg.name === argName) &&
-      ((typeof arg.value === "object") && ("type" in arg.value))
-    )?.value as DiscordChannel | undefined;
-  }
-
-  getRole(argName: string): DiscordRole | undefined {
-    return this.raw?.find((arg) =>
-      (arg.name === argName) &&
-      ((typeof arg.value === "object") && ("color" in arg.value))
-    )?.value as DiscordRole | undefined;
   }
 }
