c3163c60b5e60fa779806cc1cf6bfc9ff52a3f0b
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-08-27 19:24:39 +0200

    feat/refactor(ArgumentParser): flatten raw options to fix an issue with sub commands
    might not be the most optimized code but it seems to work

--- src/internals/structures/context/ArgumentParser.ts
+++ src/internals/structures/context/ArgumentParser.ts
@@ -1,22 +1,45 @@
-import { InteractionDataOption } from "deps";
+import { ApplicationCommandOptionTypes, InteractionDataOption } from "deps";
 
 export class ArgumentParser {
-  constructor(public raw: InteractionDataOption[] | undefined) {}
+  flattenedRawOptions: InteractionDataOption[] | undefined;
+
+  constructor(public rawOptions: InteractionDataOption[] | undefined) {
+    this.flattenedRawOptions = rawOptions;
+
+    this.recursiveFlattenOptions(rawOptions);
+  }
+
+  recursiveFlattenOptions(options: InteractionDataOption[] | undefined) {
+    if (!options || !options[0]) return;
+    switch (options[0].type) {
+      case ApplicationCommandOptionTypes.SubCommandGroup:
+        if (options[0].options) {
+          this.flattenedRawOptions?.push(...options[0].options);
+          this.recursiveFlattenOptions(options[0].options);
+        }
+        break;
+      case ApplicationCommandOptionTypes.SubCommand:
+        if (options[0].options) {
+          this.flattenedRawOptions?.push(...options[0].options);
+        }
+        break;
+    }
+  }
 
   getString(argName: string): string | undefined {
-    return this.raw?.find((arg) =>
+    return this.flattenedRawOptions?.find((arg) =>
       (arg.name === argName) && (typeof arg.value === "string")
     )?.value as string | undefined;
   }
 
   getNumber(argName: string): number | undefined {
-    return this.raw?.find((arg) =>
+    return this.flattenedRawOptions?.find((arg) =>
       (arg.name === argName) && (typeof arg.value === "number")
     )?.value as number | undefined;
   }
 
   getBoolean(argName: string): boolean | undefined {
-    return this.raw?.find((arg) =>
+    return this.flattenedRawOptions?.find((arg) =>
       (arg.name === argName) && (typeof arg.value === "boolean")
     )?.value as boolean | undefined;
   }
