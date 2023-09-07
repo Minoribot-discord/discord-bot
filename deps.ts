9d545d185acfbccc85b4de4d9ea1e3d377203abe
AuraCatPerpinya <136636937+AuraCatPerpinya@users.noreply.github.com>
2023-09-07 16:58:23 +0200

    deps: bump openweather_apis_wrapper from 0.4.3 to 0.4.4
    fixes an issue related to cache

--- deps.ts
+++ deps.ts
@@ -3,8 +3,8 @@ export * as log from "https://deno.land/std@0.192.0/log/mod.ts";
 export * as dotenv from "https://deno.land/std@0.192.0/dotenv/mod.ts";
 export * as flags from "https://deno.land/std@0.192.0/flags/mod.ts";
 export * as toml from "https://deno.land/std@0.192.0/toml/mod.ts";
-export * from "https://deno.land/x/openweather_apis_wrapper@v0.4.3/mod.ts";
-export * from "https://deno.land/x/openweather_apis_wrapper@v0.4.3/test_schemas.ts";
+export * from "https://deno.land/x/openweather_apis_wrapper@v0.4.4/mod.ts";
+export * from "https://deno.land/x/openweather_apis_wrapper@v0.4.4/test_schemas.ts";
 
 export * from "npm:@discordeno/utils@19.0.0-next.2d0f76e";
 export * from "npm:@discordeno/bot@19.0.0-next.2d0f76e";
